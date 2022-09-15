9d837e74fd6367abbcaa1f05384a8be6e5a9f2de
emilianopomar03 <99359638+emilianopomar03@users.noreply.github.com>
2022-09-15 17:30:27 -0500

    De ser aplicado, este commit enviará un mensaje a los jugadores cada vez que un cuadro haya sido ocupado.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,15 +1,17 @@
 """
 Juego Tic Tac Toe A01709338 Version 3
-En este commit se modifica el tamaño, color, y centrado de los símbolos X y O
+De ser aplicado, este commit enviará un mensaje a los jugadores cada vez que un cuadro haya sido ocupado.
 """
 
-"Se importan las librerías de turtle graphics y freegames "
+from freegames import line
+from turtle import *
 import freegames
-from turtle import * 
+"Se importan las librerías de turtle graphics y freegames "
 
-from freegames import line 
 
 "Dibujar la tabla de juego compuesta de 9 cuadros."
+
+
 def grid():
 
     line(-65, 200, -65, -200)
@@ -17,11 +19,14 @@ def grid():
     line(-200, -65, 200, -65)
     line(-200, 65, 200, 65)
 
+
 "Dibujar el jugador de x en color azul"
+
+
 def drawx(x, y):
     color("blue")
     line(x, y, x + 130, y + 130)
-    color("blue") 
+    color("blue")
     line(x, y + 130, x + 130, y)
     color("blue")
 
@@ -32,12 +37,12 @@ def drawo(x, y):
     goto(x + 65, y + 25)
     down()
     circle(40)
-    
 
 
 def floor(value):
     "Valor de círculo O en los cuadros con valor de 133"
     return ((value + 200) // 135) * 135 - 200
+
 
 "Estatus de jugador al ejecutar y lista de jugadores X y O"
 state = {'player': 0}
@@ -45,8 +50,9 @@ players = [drawx, drawo]
 
 
 "Dibujar X y O en el cuadro seleccionado"
+
+
 def tap(x, y):
-    
     x = floor(x)
     y = floor(y)
     player = state['player']
@@ -54,17 +60,25 @@ def tap(x, y):
     draw(x, y)
     update()
     state['player'] = not player
+    "Avisar que el cuadro ha sido ocupado una vez que se dibuja la figura."
+    textinput("Cuadro ocupado.", "Inténtelo de nuevo.")
 
 
-"Dimensiones"
+"Dimensiones de la cuadrícula."
 setup(450, 450, 320, 50)
-"Turtle graphics"
+
+"Oculta la tortuga de la paquetería de gráficos"
 hideturtle()
-"Rastrear ejecución de turtle graphics"
+
+"Acelerar la animación de los dibujos y delay."
 tracer(False)
+
 "Funciones de turtle graphics para la cuadrícula"
 grid()
-"Funcion para click en pantalla"
+
+"Función para refrescar la animación o dibujo. "
 update()
+
+"Función para activar el programa al dar click en pantalla"
 onscreenclick(tap)
 done()
