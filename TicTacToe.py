ab30917bd44b9d84a7eb285a9eeb906b38486621
emilianopomar03 <99359638+emilianopomar03@users.noreply.github.com>
2022-09-15 17:24:47 -0500

    Código comentado y librerías agregadas

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,32 +1,30 @@
-"""Tic Tac Toe
-Exercises
-1. Give the X and O a different color and width.
-2. What happens when someone taps a taken spot?
-3. How would you detect when someone has won?
-4. How could you create a computer player?
+"""Juego Tic Tac Toe A01709338 Version 2
+En este commit se comenta el código y se agregan librerías.
 """
 
-from turtle import *
+"Se importan las librerías de turtle graphics y freegames "
+import freegames
+from turtle import * 
 
-from freegames import line
+from freegames import line 
 
-
+"Dibujar la tabla de juego compuesta de 9 cuadros."
 def grid():
-    """Draw tic-tac-toe grid."""
+
     line(-67, 200, -67, -200)
     line(67, 200, 67, -200)
     line(-200, -67, 200, -67)
     line(-200, 67, 200, 67)
 
-
+"Dibujar el jugador de x"
 def drawx(x, y):
-    """Draw X player."""
+    
     line(x, y, x + 133, y + 133)
     line(x, y + 133, x + 133, y)
 
 
 def drawo(x, y):
-    """Draw O player."""
+    "Dibujar el jugador de O"
     up()
     goto(x + 67, y + 5)
     down()
@@ -34,16 +32,17 @@ def drawo(x, y):
 
 
 def floor(value):
-    """Round value down to grid with square size 133."""
+    "Valor de círculo O en los cuadros con valor de 133"
     return ((value + 200) // 133) * 133 - 200
 
-
+"Estatus de jugador al ejecutar y lista de jugadores X y O"
 state = {'player': 0}
 players = [drawx, drawo]
 
 
+"Dibujar X y O en el cuadro seleccionado"
 def tap(x, y):
-    """Draw X or O in tapped square."""
+    
     x = floor(x)
     y = floor(y)
     player = state['player']
@@ -52,11 +51,15 @@ def tap(x, y):
     update()
     state['player'] = not player
 
-
+"Dimensiones"
 setup(420, 420, 370, 0)
+"Turtle graphics"
 hideturtle()
+"Rastrear ejecución de turtle graphics"
 tracer(False)
+"Funciones de turtle graphics para la cuadrícula"
 grid()
+"Funcion para click en pantalla"
 update()
 onscreenclick(tap)
 done()
